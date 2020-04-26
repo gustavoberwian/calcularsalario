db750037ef7fbd17a7b8d53a78c7ba13307dec95
gustavoberwian <turmadobairro8@gmail.com>
2020-04-26 15:03:45 -0300

    site feito em PHP para calcular salario liquido de uma pessoa

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculador Salário</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <div id="logo"><img src="./images/rf.png" alt="Receita Federal"></div>
+        <h1>Calcular salário</h1>
+    </header>
+    <main>
+        <form action="./php/action.php" method="POST">
+            <input name="name" type="text" pattern="^(?![ ])(?!.*[ ]{2})((?:e|da|do|das|dos|de|d'|D'|la|las|el|los)\s*?|(?:[A-Z][^\s]*\s*?)(?!.*[ ]$))+$" required placeholder="Digite seu nome..."/><br>
+            <input name="salary" type="number" pattern="[0-9]+$" required placeholder="Digite seu salário..."/><br>
+            <input type="submit" name="Enviar" value="Enviar"/>
+        </form>
+        <div id="table">
+            <?php 
+                $dados = file_get_contents("arquivo.txt");
+                $dadosFormatados = explode("|", $dados);
+            ?>
+            <table>
+                <th id="nometh">Nome</th>
+                <th id="salarioth">Salário Líquido</th>
+                <?php                     
+                    foreach($dadosFormatados as $key => $value){    
+                        if($key%2==0){
+                            echo("<tr><td>".$value."</td>");
+                        }
+                        else{
+                            echo("<td>".$value."</td></tr>");
+                        }
+                    }   
+                ?>
+            </table>
+        </div>
+        <form action="./php/action.php" method="POST">"
+            <div id="limpar">
+                <input type="submit" name="Limpar" value="Limpar" />
+            </div>  
+        </form>              
+    </main>
+</body>
+</html> 
---
+++ php/action.php
@@ -0,0 +1,51 @@
+<?php 
+    if(isset($_POST['Enviar'])){
+        $name = $_POST['name'];
+        $salarioBruto = $_POST['salary'];
+        $arquivo = fopen("../arquivo.txt","a+");
+
+        if($salarioBruto<=1045){
+            $salarioINSS = $salarioBruto-($salarioBruto*0.075);
+        }
+        else if($salarioBruto>1045.01 && $salarioBruto<=2089.6){
+            $salarioINSS = $salarioBruto-($salarioBruto*0.09);
+        }
+        else if($salarioBruto>2089.6 && $salarioBruto<=3134.4){
+            $salarioINSS = $salarioBruto-($salarioBruto*0.12);
+        }
+        else if($salarioBruto>3134.4 && $salarioBruto<=6101.06){
+            $salarioINSS = $salarioBruto-($salarioBruto*0.14);
+        }
+        else if($salarioBruto>6101.06){
+            $salarioINSS = $salarioBruto-671.11;
+        }
+
+        if($salarioINSS<=1903.98){
+            $salarioLiquido = $salarioINSS;
+        }
+        else if($salarioINSS>1903.98 && $salarioINSS<=2826.65){
+            $salarioLiquido = $salarioINSS-($salarioINSS*0.075)+142.8;
+        }
+        else if($salarioINSS>2826.65 && $salarioINSS<=3751.05){
+            $salarioLiquido = $salarioINSS-($salarioINSS*0.15)+354.8;
+        }
+        else if($salarioINSS>3751.05 && $salarioINSS<=4664.68){
+            $salarioLiquido = $salarioINSS-($salarioINSS*0.225)+636.13;
+        }
+        else if($salarioINSS>4664.68){
+            $salarioLiquido = $salarioINSS-($salarioINSS*0.275)+869.36;
+        }
+
+        fwrite($arquivo, "Nome: ".$name." | Renda líquida: ".$salarioLiquido." |\r\n<hr/><hr/>");
+     
+        fclose($arquivo);
+        $exibirArq = fopen("../arquivo.txt","r");
+        header("Location: ../index.php");
+    }
+    if(isset($_POST['Limpar'])){
+        unlink('../arquivo.txt');
+            fopen('../arquivo.txt','w+');
+
+        header("Location: ../index.php");
+    }
+?>
